c065240556168b495ed071c2ac204a8a05548115
Livio-V <514239@vistacollege.nl>
2023-05-09 13:41:43 +0200

    update server.js etc.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 8383
+
+const mysql = require('mysql')
+
+app.use(express.static('public'))
+
+var connection = mysql.createConnection({multipleStatements: true});
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "username",
+  password: "password",
+  database: "database"
+ });
+
+
+app.get('/reservering', (req, res) => {
+  res.status(200).json("Request OK")
+})
+
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
