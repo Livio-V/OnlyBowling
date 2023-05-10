e05dbc909aa4dc3664d78fcfd69518c752b2b5d2
Livio-V <118729490+Livio-V@users.noreply.github.com>
2023-05-10 09:44:38 +0200

    Made /reservering/aanmaken endpoint

--- server.js
+++ server.js
@@ -16,12 +16,21 @@ var con = mysql.createConnection({
  });
 
 
-app.get('/reservering', (req, res) => {
+app.get('/reservering/informatie', (req, res) => {
   res.status(200).json("Request OK")
 })
 
-app.get('/test', function(req, res) {
-  res.status(200).json("Request OK")
+app.post('/reservering/aanmaken', function(req,res) {
+    
+  var sql = `INSERT INTO 'reservations'( 'fullname', 'slot', 'people', 'lane', 'tel', 'mail', 'date') VALUES ('${fullname}','${slot}','${people}','${lane}', '${tel}', '${mail}', '${date}')`
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          res.status(200).json(`Gelukt! Je reserving staat op ${date}, om ${slot}`)
+        });
+      });
 });
 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
