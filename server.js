1a9cfa4ff519e38ed0f6a2708525304201081cbf
Livio-V <118729490+Livio-V@users.noreply.github.com>
2023-05-10 11:52:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,38 @@ var con = mysql.createConnection({
   database: "database"
  });
 
+ 
 
-app.get('/reservering/informatie', (req, res) => {
-  res.status(200).json("Request OK")
+
+app.get('/reservering/status', (req, res) => {
+
+  const currentDate = new Date();
+
+  const currentHour = currentDate.getHours();
+  const currentMinute = currentDate.getMinutes();
+
+  var time = currentHour + ":" + currentMinute;
+
+    if(time = "09:00"){
+      var current_timeslot = "slot-1"
+    }
+
+    if (time > "10:00"){
+      var current_timeslot = "slot-2"
+    }
+
+    else{
+      var current_timeslot = "slot-3"
+    }
+  
+  var sql = "SELECT * FROM `reservations` WHERE date = '2023-05-09' & slot = 'slot-1'"
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      res.status(200).json(result)
+    });
+  });
 })
 
 app.post('/reservering/aanmaken', function(req,res) {
