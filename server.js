7388f5121566c50637a74a09e02c3c63dd053e9b
Livio-V <514239@vistacollege.nl>
2023-05-22 09:44:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -105,40 +105,72 @@ app.get('/reservering/status', (req, res) => {
 
   let slot; 
   if(time_reservation == 14){
-    slot = "`slot1`"
+    slot = "slot1"
   }
   else if(time_reservation == 15){
-    slot = "`slot2`"
+    slot = "slot2"
   }
   else if(time_reservation == 16){
-    slot = "`slot3`"
+    slot = "slot3"
   }
   else if(time_reservation == 17){
-    slot = "`slot4`"
+    slot = "slot4"
   }
   else if(time_reservation == 18){
-    slot = "`slot5`"
+    slot = "slot5"
   }
   else if(time_reservation == 20){
-    slot = "`slot6`"
+    slot = "slot6"
   }
   else if(time_reservation == 21){
-    slot = "`slot7`"
+    slot = "slot7"
   }
   else if(time_reservation == 22){
-    slot = "`slot8`"
+    slot = "slot8"
   }
   else {}
 
-  // Check wich lanes are avaliable with selected time slot
-  var sql = "SELECT * FROM lane1 WHERE "+ slot+ "= 'open' AND `date` = "+ formattedDate
-  
-  // Make the SQL Query to the database
-  con.query(sql, function (err, result){
-    if (err) throw err;
-    console.log(result)
-  });
+  // Define an array of lane names to check
+  var lanesToCheck = ['lane1', 'lane2', 'lane3', 'lane4', 'lane5', 'lane6', 'lane7', 'lane8']; // Add more lane names as needed
 
+  // Flag to track if an open slot is found
+  var openSlotFound = false;
+
+  // Loop through the lanes to check
+  for (var i = 0; i < lanesToCheck.length; i++) {
+  var lane = lanesToCheck[i];
+  
+  // Query the current lane
+  var sqlQuery = "SELECT ?? FROM ?? WHERE ?? = 'open' AND DATE(date) = ?;"
+  var sqlParams = [ slot, lane, slot, formattedDate ]
+  
+  // Execute the query
+  connection.query(sqlQuery, sqlParams, function (error, results, fields) {
+    if (error) {
+      // Handle the error
+      console.error(error);
+    } else {
+      // Check if an open slot is found in the current lane
+      if (results.length > 0 && results[0].slot1 === 'open') {
+        // Open slot found, perform desired operations
+        console.log('Open slot found in ' + lane);
+        openSlotFound = true;
+      } else {
+        // Open slot not found, proceed to the next lane
+        console.log('No open slot found in ' + lane);
+      }
+    }
+
+    // Check if an open slot is found or if all lanes have been checked
+    if (openSlotFound || i === lanesToCheck.length - 1) {
+      if (!openSlotFound) {
+        console.log('No open slot found in any lane.');
+      }
+      // Perform any further actions based on the result
+      // ...
+    }
+  });
+  }
   // Define the SQL params
  var sqlParams1 = [ fullname, email, phone, people, slot, date_time_reservation, date_added ]
  var sql1 = "INSERT INTO `reservations`(`fullname`, `email`, `phone`, `people`, `date_time_reservation`, `date_added`) VALUES ('?','?','?','?','?')"
